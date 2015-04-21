f76b154a08850bd25f1def7bce74992ae1525d38
唱哥 <pengchang@live.cn>
2015-04-21 10:01:29 +0800

    1

--- clean lj.bat
+++ clean lj.bat
@@ -5,7 +5,7 @@ ECHO 	项目多余文件清理, 像是 *.dlg *.obj
 
 PAUSE
 
-DEL /f /s /a h *.ncb *.plg *.obj *.pch *.pbi *.pbt *._xe *.ilk *.orig *.idb *.user *.sdf *.suo *.dep *.sdf *.tlog
+DEL /f /s /a h *.ncb *.plg *.obj *.pch *.pbi *.pbt *._xe *.ilk *.orig *.idb *.user *.sdf *.suo *.dep *.sdf *.tlog *.vcproj
 
 
 
